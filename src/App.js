fa5b55ced3430342bdf5d68f0db4de6a71b3666c
michaelcounseller <michaelcounseller@gmail.com>
2022-11-09 15:21:43 -0600

    adding a navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import Carousel from './components/Carousel';
+import Carousel from "./components/Carousel";
+import * as React from "react";
+import { AppBar, Box, IconButton, Link, Toolbar, Typography } from "@mui/material";
+import { AccountCircleRounded, Menu } from "@mui/icons-material";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";'
 
 export default function App() {
-    return (
-        <>
-        <Carousel/>
-          <p>Testing hello</p> 
-        </>
-    )
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="transparent">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <a href="https://github.com/blopDev">
+
+              <AccountCircleRounded fontSize="large" color="info"/>
+              </a>
+              
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              Michael Counseller
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Carousel />
+      <p>Testing hello</p>
+    </>
+  );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ export default function Item({item})
     return (
         <Paper justifyContent="center" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <h2>{item.key}</h2>
-            <img justifyContent="center" src={item.image} alt={item.key} style={{ width: "50%" }}/>
+            <img justifyContent="center" src={item.image} alt={item.key} style={{ width: "50%", height: "auto" }}/>
             <p>{item.desc}</p>
 
             <Button className="CheckButton">
